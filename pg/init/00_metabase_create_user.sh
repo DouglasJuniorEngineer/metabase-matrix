cfc5a9f44cbf58b61efc6e50565b3c0015cacc64
Douglas Rosa Tavares Júnior <douglastavaresjunior@gmail.com>
2024-09-04 11:09:19 -0300

    douglas

--- pg/init/00_metabase_create_user.sh
+++ pg/init/00_metabase_create_user.sh
@@ -3,7 +3,7 @@ set -e # exit if a command exits with a not-zero exit code
 
 POSTGRES="psql -U postgres"
 
-# create a shared role to read & write general datasets into postgres
+#. create a shared role to read & write general datasets into postgres
 echo "Creating database role: postgres"
 $POSTGRES <<-EOSQL
 CREATE USER postgres WITH
